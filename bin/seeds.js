14f55b8913ac3cf0c9b8da3629e1090cb06fcbe9
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-08 18:52:34 +0000

    seeds.js code simplified

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,12 +1,13 @@
 // Iteration #1
 const mongoose = require('mongoose');
 const Drone = require('../models/Drone.model');
-const DB_NAME = 'express-drones-dev';
-mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// const DB_NAME = 'express-drones-dev';
+// mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+require('../configs/db.config');
 
 const drones = [
     { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
@@ -17,6 +18,7 @@ const drones = [
 async function createDrone() {
     try {
         await Drone.create(drones);
+        console.log('Drones inserted!');
         mongoose.connection.close();
     } catch (error) {
         console.log(`Error while inserting drones ${error}`);
